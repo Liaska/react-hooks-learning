f45c4cae588fe0f80b2665d8a8dfa6e8d19cd4f5
Liaska Konstantin <kostyalyaska@yandex.ru>
2020-12-12 12:59:19 +0300

    hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  function increment() {
+    setCounter(prev => prev + 1) 
+  }
+
+  function decrement() {
+    setCounter(prev => prev - 1) 
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Счетчик: {counter}</h1>
+      <button onClick={increment} className="btn btn-success">Добавить</button>
+      <button onClick={decrement} className="btn btn-danger">Убрать</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
